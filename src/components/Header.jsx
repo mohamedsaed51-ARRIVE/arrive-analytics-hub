4b6fa383b019c700bb95c96435c17c0800a60ca7
Mohamed Saeed <mohamedsaed51@gmail.com>
2026-07-31 05:28:58 +0300

    ARRIVE Hub V3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,79 @@
-import { AppBar, Toolbar, Typography, Avatar, Box } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Box,
+  TextField,
+  InputAdornment
+} from "@mui/material";
 
-export default function Header() {
+import SearchIcon from "@mui/icons-material/Search";
+
+import logo from "../assets/arrive-logo.png";
+
+export default function Header({ search, setSearch }) {
   return (
     <AppBar
-      position="fixed"
-      elevation={1}
+      position="sticky"
+      elevation={0}
       sx={{
-        bgcolor: "#fff",
-        color: "#111827",
-        zIndex: 1300,
+        background: "#fff",
+        borderBottom: "3px solid #07269B"
       }}
     >
-      <Toolbar>
-        <Typography variant="h6" fontWeight="bold" sx={{ flexGrow: 1 }}>
-          ARRIVE Analytics Hub
-        </Typography>
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          gap: 4
+        }}
+      >
+        <Box
+          display="flex"
+          alignItems="center"
+          gap={2}
+        >
+          <img
+            src={logo}
+            alt="ARRIVE"
+            style={{
+              height: 42
+            }}
+          />
 
-        <Box display="flex" alignItems="center" gap={2}>
-          <Typography>Mohamed</Typography>
-          <Avatar sx={{ bgcolor: "#1565C0" }}>M</Avatar>
+          <Box>
+            <Typography
+              fontWeight={700}
+              color="#07269B"
+            >
+              Analytics Hub
+            </Typography>
+
+            <Typography
+              fontSize={12}
+              color="gray"
+            >
+              Executive Control Center
+            </Typography>
+          </Box>
         </Box>
+
+        <TextField
+          placeholder="Search dashboard..."
+          value={search}
+          onChange={(e)=>setSearch(e.target.value)}
+          size="small"
+          sx={{
+            width: 340
+          }}
+          InputProps={{
+            startAdornment:(
+              <InputAdornment position="start">
+                <SearchIcon/>
+              </InputAdornment>
+            )
+          }}
+        />
       </Toolbar>
     </AppBar>
   );
